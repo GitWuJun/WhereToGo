9b34489f651407c2de5af8545e528861cf304183
wujun <790338959@qq.com>
2018-07-23 14:17:25 +0800

    test

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -9,7 +9,7 @@
 				<div class="item-info">
 					<p class="item-title">{{item.title}}</p>
 					<p class="item-desc">{{item.desc}}</p>
-					<button class="item-button">查看详情</button>
+					<button class="item-button">查看详情text</button>
 				</div>
 			</li>
 
